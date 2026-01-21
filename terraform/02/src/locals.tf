b77d98a83f9df001c10d4aa9c5792536ad8374da
VladislavCheshenko <35314744+CheshenkoVladislav@users.noreply.github.com>
2026-01-21 12:33:13 +0300

    Update locals.tf

--- terraform/02/src/locals.tf
+++ terraform/02/src/locals.tf
@@ -1,6 +1,7 @@
 locals {
-  vm_web_name = "${var.vm_web_name}"
-  vm_db_name  = "${var.vm_db_name}"
+  vm_web_name = var.vm_web_name
+  vm_db_name  = var.vm_db_name
+  interpolated = "${vm_db_name}, ${vm_web_name}"
 
   metadata = {
     serial-port-enable = 1
