b66aebc437cb10b54afe7ff5228a828fc48a4449
Cheshenko Vladislav <merinoplaid@gmail.com>
2026-01-24 14:26:50 +0300

    hw fix

--- terraform/02/src/locals.tf
+++ terraform/02/src/locals.tf
@@ -1,7 +1,7 @@
 locals {
-  vm_commom = "netology-develop-platform"
-  vm_web_name = "${vm_common}-${var.vm_web_name}"
-  vm_db_name  = "${vm_common}-${var.vm_db_name}"
+  vm_common = "netology-develop-platform"
+  vm_web_name = "${local.vm_common}-${var.vm_web_name}"
+  vm_db_name  = "${local.vm_common}-${var.vm_db_name}"
 
   metadata = {
     serial-port-enable = 1
