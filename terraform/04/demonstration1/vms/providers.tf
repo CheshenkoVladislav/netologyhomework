619334c75e13cdc3d806a7ff33c442ebcee6dfe4
Cheshenko Vladislav <merinoplaid@gmail.com>
2026-01-29 21:16:57 +0300

    add s3

--- terraform/04/demonstration1/vms/providers.tf
+++ terraform/04/demonstration1/vms/providers.tf
@@ -5,6 +5,25 @@ terraform {
     }
   }
   required_version = "~>1.13.0"
+
+  backend "s3" {
+    bucket  = "b1gv6lhe0hp2uq4030l5-bucket"
+    key     = "terraform.tfstate"
+    region  = "ru-central1"
+    
+    # Встроенный механизм блокировок (Terraform >= 1.6)
+    # Не требует отдельной базы данных!
+    use_lockfile = true
+    
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+    
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+  }
 }
 
 provider "yandex" {
