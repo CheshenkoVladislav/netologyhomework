74e33d5f7bf51fdf67da619720d51ec149bbb00c
CheshenkoVladislav <merinoplaid@gmail.com>
2026-06-17 23:08:25 +0300

    HW3

---
+++ network/hw1/encrypt.tf
@@ -0,0 +1,12 @@
+resource "yandex_kms_symmetric_key" "my_key" {
+  name = "asymmetric-encryption-key-name"
+}
+
+resource "yandex_kms_symmetric_key_iam_binding" "access_rights" {
+    symmetric_key_id = yandex_kms_symmetric_key.my_key.id
+    role = "kms.keys.encrypterDecrypter"
+
+    members = [
+        "userAccount:${yandex_iam_service_account.netology-service-account.id}"
+    ]
+}
--- network/hw1/main.tf
+++ network/hw1/main.tf
@@ -14,16 +14,16 @@ provider "yandex" {
   token     = var.token
 }
 
-resource "yandex_vpc_network" "network" {
-  name = "network"
-}
+# resource "yandex_vpc_network" "network" {
+#   name = "network"
+# }
 
-resource "yandex_vpc_subnet" "public" {
-  name           = "public"
-  zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.network.id
-  v4_cidr_blocks = ["192.168.10.0/24"]
-}
+# resource "yandex_vpc_subnet" "public" {
+#   name           = "public"
+#   zone           = "ru-central1-a"
+#   network_id     = yandex_vpc_network.network.id
+#   v4_cidr_blocks = ["192.168.10.0/24"]
+# }
 
 # resource "yandex_vpc_subnet" "private" {
 #   name           = "private"
@@ -98,6 +98,15 @@ resource "yandex_vpc_subnet" "public" {
 resource "yandex_storage_bucket" "iam-bucket" {
   bucket    = "netology-cheshenko"
   folder_id = var.folder_id
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = yandex_kms_symmetric_key.my_key.id
+        sse_algorithm = "aws:kms"
+      }
+    }
+  }
 }
 
 resource "yandex_storage_object" "my_file" {
@@ -132,99 +141,99 @@ resource "yandex_resourcemanager_folder_iam_binding" "folder-editor-binding" {
 }
 
 # Instance Group
-resource "yandex_compute_instance_group" "web" {
-  name                = "test-ig"
-  folder_id           = var.folder_id
-  service_account_id  = yandex_iam_service_account.netology-service-account.id
-  deletion_protection = false
-  instance_template {
-    platform_id = "standard-v1"
-    resources {
-      memory        = 2
-      cores         = 2
-      core_fraction = 20
-    }
-    boot_disk {
-      mode = "READ_WRITE"
-      initialize_params {
-        image_id = "fd827b91d99psvq5fjit"
-        size     = 4
-      }
-    }
+# resource "yandex_compute_instance_group" "web" {
+#   name                = "test-ig"
+#   folder_id           = var.folder_id
+#   service_account_id  = yandex_iam_service_account.netology-service-account.id
+#   deletion_protection = false
+#   instance_template {
+#     platform_id = "standard-v1"
+#     resources {
+#       memory        = 2
+#       cores         = 2
+#       core_fraction = 20
+#     }
+#     boot_disk {
+#       mode = "READ_WRITE"
+#       initialize_params {
+#         image_id = "fd827b91d99psvq5fjit"
+#         size     = 4
+#       }
+#     }
 
-    scheduling_policy {
-      preemptible = true
-    }
-    network_interface {
-      network_id = yandex_vpc_network.network.id
-      subnet_ids = ["${yandex_vpc_subnet.public.id}"]
-    }
-    labels = {
-      type = "web-public-vm"
-    }
-    metadata = {
-      user-data = templatefile("${path.module}/cloud_config.yaml.tftpl", {
-        index = indent(6, file("${path.module}/index.html"))
-      })
-    }
-  }
+#     scheduling_policy {
+#       preemptible = true
+#     }
+#     network_interface {
+#       network_id = yandex_vpc_network.network.id
+#       subnet_ids = ["${yandex_vpc_subnet.public.id}"]
+#     }
+#     labels = {
+#       type = "web-public-vm"
+#     }
+#     metadata = {
+#       user-data = templatefile("${path.module}/cloud_config.yaml.tftpl", {
+#         index = indent(6, file("${path.module}/index.html"))
+#       })
+#     }
+#   }
 
-  health_check {
-    healthy_threshold   = 2
-    interval            = 60
-    timeout             = 10
-    unhealthy_threshold = 3
-    http_options {
-      path = "/"
-      port = 80
-    }
-  }
+#   health_check {
+#     healthy_threshold   = 2
+#     interval            = 60
+#     timeout             = 10
+#     unhealthy_threshold = 3
+#     http_options {
+#       path = "/"
+#       port = 80
+#     }
+#   }
 
-  scale_policy {
-    fixed_scale {
-      size = 2
-    }
-  }
+#   scale_policy {
+#     fixed_scale {
+#       size = 2
+#     }
+#   }
 
-  allocation_policy {
-    zones = ["ru-central1-a"]
-  }
+#   allocation_policy {
+#     zones = ["ru-central1-a"]
+#   }
 
-  deploy_policy {
-    max_unavailable = 1
-    max_creating    = 2
-    max_expansion   = 2
-    max_deleting    = 2
-  }
-}
+#   deploy_policy {
+#     max_unavailable = 1
+#     max_creating    = 2
+#     max_expansion   = 2
+#     max_deleting    = 2
+#   }
+# }
 
-resource "yandex_lb_target_group" "web_tg" {
-  name      = "web-target-group"
-  folder_id = var.folder_id
+# resource "yandex_lb_target_group" "web_tg" {
+#   name      = "web-target-group"
+#   folder_id = var.folder_id
 
-  dynamic "target" {
-    for_each = yandex_compute_instance_group.web.instances
-    content {
-      subnet_id = target.value.network_interface[0].subnet_id
-      address   = target.value.network_interface[0].ip_address
-    }
-  }
-}
+#   dynamic "target" {
+#     for_each = yandex_compute_instance_group.web.instances
+#     content {
+#       subnet_id = target.value.network_interface[0].subnet_id
+#       address   = target.value.network_interface[0].ip_address
+#     }
+#   }
+# }
 
-resource "yandex_lb_network_load_balancer" "web_balancer" {
-  name = "web-balancer"
-  listener {
-    name = "http-listener"
-    port = 80
-  }
-  attached_target_group {
-    target_group_id = yandex_lb_target_group.web_tg.id
-    healthcheck {
-      name = "web-check"
-      http_options {
-        port = 80
-        path = "/"
-      }
-    }
-  }
-}
+# resource "yandex_lb_network_load_balancer" "web_balancer" {
+#   name = "web-balancer"
+#   listener {
+#     name = "http-listener"
+#     port = 80
+#   }
+#   attached_target_group {
+#     target_group_id = yandex_lb_target_group.web_tg.id
+#     healthcheck {
+#       name = "web-check"
+#       http_options {
+#         port = 80
+#         path = "/"
+#       }
+#     }
+#   }
+# }
