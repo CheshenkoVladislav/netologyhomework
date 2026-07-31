def3d13f7cae813aa762558dfd4d42e656d5a375
CheshenkoVK <merinoplaid@gmail.com>
2026-07-31 22:59:08 +0300

    grafana+myapp

--- diplom/main/alb.tf
+++ diplom/main/alb.tf
@@ -1,26 +1,41 @@
-# resource "null_resource" "init-kubectl-config" {
-#   depends_on = [yandex_kubernetes_cluster.regional_cluster]
+resource "null_resource" "init-kubectl-config" {
+  depends_on = [yandex_kubernetes_cluster.regional_cluster]
 
-#   provisioner "local-exec" {
-#     command = "yc managed-kubernetes cluster get-credentials ${yandex_kubernetes_cluster.regional_cluster.id} --external --force"
-#   }
-# }
+  provisioner "local-exec" {
+    command = "yc managed-kubernetes cluster get-credentials ${yandex_kubernetes_cluster.regional_cluster.id} --external --force"
+  }
+}
 
-# resource "null_resource" "run_script" {
-#   depends_on = [null_resource.init-kubectl-config, yandex_kubernetes_node_group.node_group]
+resource "null_resource" "cleanup_gateway_api" {
+  provisioner "local-exec" {
+    when    = create
+    command = <<-EOT
+      kubectl delete validatingadmissionpolicy safe-upgrades.gateway.networking.k8s.io --ignore-not-found
+      kubectl delete validatingadmissionpolicybinding safe-upgrades.gateway.networking.k8s.io --ignore-not-found
+      kubectl delete crd gatewayclasses.gateway.networking.k8s.io --ignore-not-found
+      kubectl delete crd gateways.gateway.networking.k8s.io --ignore-not-found
+      kubectl delete crd httproutes.gateway.networking.k8s.io --ignore-not-found
+      kubectl delete crd referencegrants.gateway.networking.k8s.io --ignore-not-found
+      sleep 3
+    EOT
+  }
+}
 
-#   provisioner "local-exec" {
-#     command = <<-EOT
-#     kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.0.0/standard-install.yaml &&
-#     helm pull oci://cr.yandex/yc-marketplace/yandex-cloud/gwin/charts/gwin-chart \
-#         --version v1.8.2 \
-#         --untar &&
-#     helm install \
-#         --namespace alb-diplom-cluster \
-#         --create-namespace \
-#         --set controller.folderId=${var.folder_id} \
-#         --set-file controller.ycServiceAccount.secret.value=./prepare.json \
-#     gwin ./gwin-chart
-#     EOT
-#   }
-# }
+resource "null_resource" "run_script" {
+  depends_on = [yandex_kubernetes_node_group.node_group, null_resource.cleanup_gateway_api]
+
+  provisioner "local-exec" {
+    command = <<-EOT
+    rm -rf gwin-chart &&
+    helm pull oci://cr.yandex/yc-marketplace/yandex-cloud/gwin/charts/gwin-chart \
+        --version v1.8.2 \
+        --untar &&
+    helm install \
+        --namespace alb-diplom-cluster \
+        --create-namespace \
+        --set controller.folderId=${var.folder_id} \
+        --set-file controller.ycServiceAccount.secret.value=./prepare.json \
+    gwin ./gwin-chart
+    EOT
+  }
+}
